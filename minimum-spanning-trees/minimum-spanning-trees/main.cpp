10e06410130ee4c327035edbdddf334413a0c1af
Samuel Lam <samuelkinlam@gmail.com>
2017-02-24 11:36:15 -0500

    add in creation 1-d matrix

--- minimum-spanning-trees/minimum-spanning-trees/main.cpp
+++ minimum-spanning-trees/minimum-spanning-trees/main.cpp
@@ -6,9 +6,66 @@
 //
 
 #include <iostream>
+#include <time.h>
+
+/*
+ * Function generates and returns an adjacency matrix
+ * @param n : # of vertices in the matrix
+ * @param dim : dimension of the matrix
+ */
+void matrix_generator(int n, int dim) {
+    srand((unsigned)time(NULL));
+    int matrix[n][n];
+    
+    if (dim == 0) {
+        // skips the diagonal and copies symmetric value of matrix to other side
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                matrix[i][j] = (double)rand() / RAND_MAX;
+                matrix[j][i] = matrix[j][i];
+            }
+        }
+    }
+    
+    else if (dim == 1) {
+        
+        int x_coords[n];
+        int y_coords[n];
+        
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                matrix[i][j] = (double)rand() / RAND_MAX;
+                matrix[j][i] = matrix[j][i];
+            }
+        }
+    }
+    
+    else if (dim == 2) {
+        
+        int x_coords[n];
+        int y_coords[n];
+        
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                matrix[i][j] = (double)rand() / RAND_MAX;
+                matrix[j][i] = matrix[j][i];
+            }
+        }
+    }
+    
+    else if (dim == 3) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                matrix[i][j] = (double)rand() / RAND_MAX;
+                matrix[j][i] = matrix[j][i];
+            }
+        }
+    }
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    matrix_generator(3, 2);
     return 0;
 }
