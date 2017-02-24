99307bb6bb6e7f7f65b62eb5dd987029c43b4cc0
Samuel Lam <samuelkinlam@gmail.com>
2017-02-24 10:20:33 -0500

    commit xcode proj

---
+++ minimum-spanning-trees/minimum-spanning-trees/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  minimum-spanning-trees
+//
+//  Created on 2/24/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
