b5e8d39bfa1196f06559aeee5353d60d4b01d97c
Samuel Lam <samuelkinlam@gmail.com>
2017-02-25 18:43:51 -0500

    fix MinHeapify

--- minimum-spanning-trees/minimum-spanning-trees/d-heap.hpp
+++ minimum-spanning-trees/minimum-spanning-trees/d-heap.hpp
@@ -2,8 +2,6 @@
 //  d-heap.hpp
 //  minimum-spanning-trees
 //
-//  Created by Samuel K. Lam on 2/25/17.
-//  Copyright © 2017 Samuel K. Lam. All rights reserved.
 //
 
 #ifndef d_heap_hpp
--- minimum-spanning-trees/minimum-spanning-trees/main.cpp
+++ minimum-spanning-trees/minimum-spanning-trees/main.cpp
@@ -131,6 +131,9 @@ private:
     int size;
     int current_size;
     int **array;
+    // we use this array to track the position of the vertex in the heap
+    // positive int denotes its position in the array, -1 if not in the heap
+    int *heap_vertex_pos;
     
 public:
     /*
@@ -143,8 +146,10 @@ public:
         current_size = 0;
         d = num_children;
         array = new int* [capacity];
-        for (int i = 0 ; i < capacity + 1; i++) {
+        heap_vertex_pos = new int[capacity];
+        for (int i = 0 ; i < capacity; i++) {
             array[i] = new int[2];
+            heap_vertex_pos[i] = -1;
         }
     }
     
@@ -177,20 +182,45 @@ public:
     }
     
     /*
-     * Function to insert element
+     * Checks to see what position the vertex is in the heap
+     */
+    int Find(int v) {
+        return (heap_vertex_pos[v] != -1) ? heap_vertex_pos[v] : -1;
+    }
+    
+    /*
+     * Swaps index position in the heap
+     */
+    void SwapIndexPositionInHeap(int *v1, int *v2, int idx1, int idx2) {
+        heap_vertex_pos[v1[0]] = idx2;
+        heap_vertex_pos[v2[0]] = idx1;
+    }
+    
+    /*
+     * Insert element into the heap and Bubble Up to maintain heap properties
      * Note that we store heap elements in the form of [vertex #, distance/weight edge]
      */
-    void insert(int vertex, int distance) {
+    void Insert(int vertex, int distance) {
         if (IsFull()) {
             std::cout << "Warning! We've reached the max heap size." << std::endl;
             return;
         }
-
-        array[current_size][0] = vertex;
-        array[current_size][1] = distance;
         
-        BubbleUp(current_size);
-        current_size++;
+        // if we already have the vertex in the heap than we just update the distance
+        if (Find(vertex) != -1) {
+            array[heap_vertex_pos[vertex]][1] = distance;
+            MinHeapify(heap_vertex_pos[vertex]);
+            BubbleUp(heap_vertex_pos[vertex]);
+        }
+        // otherwise do our standard insert
+        else {
+            array[current_size][0] = vertex;
+            array[current_size][1] = distance;
+            heap_vertex_pos[vertex] = current_size;
+            
+            BubbleUp(current_size);
+            current_size++;
+        }
     }
     
     /*
@@ -198,9 +228,10 @@ public:
      */
     void BubbleUp(int insert_pos) {
         while (insert_pos > 0 && array[insert_pos][1] < array[GetParent(insert_pos)][1]) {
-            int parent = GetParent(insert_pos);
-            std::swap(array[insert_pos], array[parent]);
-            insert_pos = parent;
+            int parent_pos = GetParent(insert_pos);
+            SwapIndexPositionInHeap(array[insert_pos], array[parent_pos], insert_pos, parent_pos);
+            std::swap(array[insert_pos], array[parent_pos]);
+            insert_pos = parent_pos;
         }
     }
 
@@ -217,6 +248,10 @@ public:
         min[0] = array[0][0];
         min[1] = array[0][1];
         
+        heap_vertex_pos[array[0][0]] = -1;
+        if (!IsEmpty()) {
+            heap_vertex_pos[array[current_size-1][0]] = 0;
+        }
         array[0] = array[current_size-1];
         current_size--;
         
@@ -231,17 +266,18 @@ public:
     void MinHeapify(int parent_pos) {
         
         int swap_pos = parent_pos;
-        bool swap_bool = 1;
+        int swap_bool = 1;
         
         while (swap_bool) {
             // iterate over the children to find the smallest value to swap with
             for (int i = 1; i < d + 1; i++) {
                 int child_pos = GetKthChild(parent_pos, i);
-                if ((child_pos < current_size) && (array[child_pos][1] < array[parent_pos][1])) {
+                if ((child_pos < current_size) && (array[child_pos][1] < array[swap_pos][1])) {
                     swap_pos = child_pos;
                 }
             }
             if (swap_pos != parent_pos) {
+                SwapIndexPositionInHeap(array[parent_pos], array[swap_pos], parent_pos, swap_pos);
                 std::swap(array[parent_pos], array[swap_pos]);
                 parent_pos = swap_pos;
             }
@@ -261,18 +297,102 @@ public:
     }
 };
 
+/*
+ * Prim's algorithm
+ * @param matrix : matrix containing the graph representation
+ * @param n : # of vertices in the graph
+ * @return sum : the sum of the weight of the MST
+ */
+template<int MAX_ROW, int MAX_COL>
+float Prim(float (&matrix)[MAX_ROW][MAX_COL], int n) {
+    int dist[n];
+    int prev[n];
+    int v[2];
+    
+    float sum = 0;
+    
+    DHeap min_heap(n, (pow(n, n-2) / n));
+    
+    // serves as our set to contain [1, 0] <-> [0th vertex is in MST, 1th vertex not in MST]
+    int vertices[n];
+    
+    for (int i = 1; i < n; i++) {
+        
+        // initialize all the vertices to not visited
+        vertices[i] = 0;
+        
+        // note that this value should be sufficiently large since the max distance (in hypercube) is less than this
+        dist[i] = 10000;
+    }
+    
+    // we will start Prim's from the 0th vertex
+    vertices[0] = 1;
+    dist[0] = 0;
+    
+    min_heap.Insert(0, dist[0]);
+    
+    while (!min_heap.IsEmpty()) {
+        
+        min_heap.DeleteMin(v);
+        
+        // mark removed vertex as visited
+        vertices[v[0]] = 1;
+        sum += dist[v[0]];
+        
+        // iterate over edges of that vertex
+        for (int w = 0; w < n; w++) {
+            // check that this vertex is not itself & we haven't visited it before
+            if ((v[0] != w) && (v[w] == 0)) {
+                if (dist[w] > matrix[v[0]][w]) {
+                    dist[w] = matrix[v[0]][w];
+                    prev[w] = v[0];
+                    min_heap.Insert(w, dist[w]);
+                }
+            }
+        }
+    }
+    
+    return sum;
+}
+
 
 int main(int argc, const char * argv[]) {
 
-//    int n = 6;
-//    int d = 3;
-//    float matrix[6][6];
+    std::cout << "Hello world!" << std::endl;
     
-//    MatrixGenerator(matrix, n, d);
+    int n = 6;
+    int d = 2;
+    float matrix[6][6];
     
-//    DHeap min_heap(11, 2);
-    
+    MatrixGenerator(matrix, n, d);
 //    MatrixPrint(matrix);
+    
+    
+//    int sum = Prim(matrix, n);
+//    
+//    std::cout << "Sum here: " <<sum << std::endl;
+    
+    // Testing for DHeap Implemetation
+    DHeap min_heap(n, 2);
+    min_heap.Insert(0, 1);
+    min_heap.Insert(1, 3);
+    min_heap.Insert(2, 2);
+    min_heap.Insert(3, 3);
+    min_heap.Insert(3, 4);
+    min_heap.Insert(4, 3);
+    min_heap.Insert(4, 0);
+    min_heap.printHeap();
+    int v[2];
+    min_heap.DeleteMin(v);
+    min_heap.printHeap();
+    min_heap.DeleteMin(v);
+    min_heap.printHeap();
+    min_heap.Insert(1, 3);
+    min_heap.Insert(2, 2);
+    min_heap.Insert(0, 3);
+    min_heap.printHeap();
+    min_heap.Insert(0, 0);
+    min_heap.printHeap();
     
     return 0;
 }
